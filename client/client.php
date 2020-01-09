1a0a7726d264939c3c4393575d47f963e7aed435
Milad Abooali <m.abooali@hotmail.com>
2020-01-09 12:41:15 +0330

    Fix: remove response for test()
    Romved function response removed.

--- client/client.php
+++ client/client.php
@@ -1,8 +1,6 @@
 <?php
    $client = new SoapClient('http://localhost/my-app/wsdl/demo.wsdl');  // Set WSDL URL
    $res    = $client->ItemQuery('4');
-   $res2    = $client->test('4');
    $count  = $res->count;
    $size   = $res->size;
    print "count=$count, size=$size\n";
-   print $res2;
